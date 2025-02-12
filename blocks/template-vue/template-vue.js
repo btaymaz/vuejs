f0d7b4f14129a61951a6f3a11aa0f1a6fd03de0f
bulent taymaz <56704328+btaymaz@users.noreply.github.com>
2025-02-12 08:14:44 -0500

    Create template-vue.js

---
+++ blocks/template-vue/template-vue.js
@@ -0,0 +1,62 @@
+import { createApp, compile, h } from "vue";
+
+export default async function decorate(block) {
+  const blockWrapper = document.createElement("div");
+  const linkData = {};
+  linkData.title = "Title Lorem";
+  linkData.description = "Sample Description";
+  linkData.link = "https://www.google.com";
+  linkData.linkText = "View all";
+  linkData.items = [
+    {
+      title: "Link Title",
+      description: "Link Description",
+      link: "https://www.google.com",
+      date: "2023-10-01",
+    },
+    {
+      title: "Link Title 2",
+      description: "Link Description 2",
+      link: "https://www.google.com",
+      date: "2023-10-02",
+    },
+    {
+      title: "Link Title 3",
+      description: "Link Description 3",
+      link: "https://www.google.com",
+      date: "2023-10-03",
+    },
+  ];
+
+  const template = `<div> 
+              <h2>{{ title }}</h2> 
+              <p>{{ description }}</p> 
+              <a v-if='link' :href='link'>{{ linkText }}</a> 
+              <ul> 
+                <li v-for='item in items' :key='item.title'> 
+                  <a :href='item.link'>{{ item.title }}</a> <p>{{ item.description }}</p> <p>{{ item.date }}</p> 
+                </li>
+              </ul>
+            </div>`;
+
+  const app = createApp({
+    render() {
+      return h(
+        {
+          props: {
+            linkData,
+          },
+          render() {
+            return h(compile(template), this.linkData);
+          },
+        },
+        { newsData: linkData }
+      );
+    },
+  });
+
+  app.mount(blockWrapper);
+
+  block.innerHTML = "";
+  block.appendChild(blockWrapper);
+}

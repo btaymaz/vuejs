59cc23ce298d6bfdb0a5aa560d4d1fccaa610a25
bulent taymaz <56704328+btaymaz@users.noreply.github.com>
2025-02-14 13:37:17 -0500

    Create template.vue

---
+++ blocks/file-vue/template.vue
@@ -0,0 +1,10 @@
+<div> 
+  <h2>{{ title }}</h2> 
+  <p>{{ description }}</p> 
+  <a v-if='link' :href='link'>{{ linkText }}</a> 
+  <ul> 
+    <li v-for='item in items' :key='item.title'> 
+      <a :href='item.link'>{{ item.title }}</a> <p>{{ item.description }}</p> <p>{{ item.date }}</p> 
+    </li>
+  </ul>
+</div>
